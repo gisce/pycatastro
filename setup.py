aa34c4fae53a2666611b8a25110cfbc0bf40c80e
Xavier Barnada <xbarnada@gisce.net>
2015-05-15 17:59:15 +0200

    Bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycatastro',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/gisce/pycatastro',
     license='GPLv3',
