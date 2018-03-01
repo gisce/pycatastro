075dbf487ab6c38eb411ff59e6baeecaf2606574
Eduard Carreras <ecarreras@gisce.net>
2018-03-01 10:56:06 +0100

    Bump to v0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycatastro',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     url='https://github.com/gisce/pycatastro',
     license='GPLv3',
